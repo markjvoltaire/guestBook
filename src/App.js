d4867f967c2dc6793a7abcee08f40c1bc62e42c3
Mark Voltaire <markjvoltaire@gmail.com>
2022-02-22 12:43:35 -0500

    private route created, authContext created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import Home from './views/Home';
 import { EntryProvider } from './context/EntryContext';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './components/Auth/Login';
 
 function App() {
   return (
     <div className="App">
       <h4 className="title">Guest Book</h4>
-      <EntryProvider>
-        <Home />
-      </EntryProvider>
+      <BrowserRouter>
+        <Switch>
+          <EntryProvider>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </EntryProvider>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <input placeholder="email" />
+        <div />
+
+        <div>
+          <input placeholder="password" />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
